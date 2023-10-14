4495849fca08f2747aa7e22f4e67fe8d1f0a85b9
frey808 <kaifrazier413@gmail.com>
2023-10-14 17:56:45 -0400

    testing server setup

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+from server.api.skate import *
+from server.db.setup import *
+
+app = Flask(__name__) #create Flask instance
+api = Api(app) #api router
+
+api.add_resource(Data,'/')
+
+if __name__ == '__main__':
+    print("Loading db")
+    rebuild()
+    print("Starting flask")
+    app.run(debug=True)
